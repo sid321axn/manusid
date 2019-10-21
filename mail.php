d4bcf310f6e0406bc67739ac83fdd7a4728b7fc4
Manu Siddhartha <sid321axn@gmail.com>
2019-10-21 23:58:49 +0530

    Add files via upload

--- mail.php
+++ mail.php
@@ -4,10 +4,10 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
 
-        $name = strip_tags(trim($_POST["cf-name"]));
+        $name = strip_tags(trim($_POST["name"]));
         $name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["cf-email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["cf-message"]);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
 
 
         // Check that data was sent to the mailer.
